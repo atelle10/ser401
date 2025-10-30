2cd996aba10711b25c624663eff02720a89175b4
lssturdi-4 <sturdiventleroy@gmail.com>
2025-10-29 20:18:00 -0400

    sidebar component complete

--- kpi_dashboard/src/Components/Logo.jsx
+++ kpi_dashboard/src/Components/Logo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className="flex items-center cursor-pointer shrink mb-2 rounded-2xl justify-center p-1 bg-white shadow-blue-500/20 shadow-md w-fit h-fit">
-        <img src="./src/Components/assets/Famar Logo.png" alt="Famar Logo" className='inline- w-15 h-15 object-centertransition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110'/>
-        <h1 className="text-center text-xs font-bold text-wrap">Fire And Medical <br /> Analytic Report</h1>
+    <div className="grid grid-cols-1 text-xs items-center cursor-pointer shrink rounded-2xl justify-center p-1 object-center bg-white shadow-blue-500/20 shadow-md w-fit h-fit">
+        <img src="./src/Components/assets/Famar Logo.png" alt="Famar Logo" className='object-center w-15 h-15 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110'/>
+        <h1 className="text-center text-xs font-bold text-wrap">Fire And Medical Analytic Report</h1>
     </div>
   )
 }
