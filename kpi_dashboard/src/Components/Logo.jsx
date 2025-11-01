48154d1e9630318a583a626b3df4293702ef81e5
lssturdi-4 <sturdiventleroy@gmail.com>
2025-10-31 20:14:55 -0400

    logo centered, sidebar icons and hover feature updated

--- kpi_dashboard/src/Components/Logo.jsx
+++ kpi_dashboard/src/Components/Logo.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className="grid grid-cols-1 text-xs items-center cursor-pointer shrink rounded-2xl justify-center p-1 object-center bg-white shadow-blue-500/20 shadow-md w-fit h-fit">
-        <img src="./src/Components/assets/Famar Logo.png" alt="Famar Logo" className='object-center w-15 h-15 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110'/>
+    <div className="grid grid-cols-1 p-0.5 text-xs items-center cursor-pointer shrink rounded-2xl justify-center bg-white shadow-blue-500/20 shadow-md w-fit h-fit">
+        <img src="./src/Components/assets/Famar Logo.png" alt="Famar Logo" className='mx-auto object-center w-15 h-15 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110'/>
         <h1 className="text-center text-xs font-bold text-wrap">Fire And Medical Analytic Report</h1>
     </div>
   )
