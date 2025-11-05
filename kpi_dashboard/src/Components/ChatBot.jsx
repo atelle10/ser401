ffabf09d973fb8ac51337de507c17ebb034903c2
lssturdi-4 <sturdiventleroy@gmail.com>
2025-11-04 23:49:24 -0500

    visual adjustments to chatbot component

--- kpi_dashboard/src/Components/ChatBot.jsx
+++ kpi_dashboard/src/Components/ChatBot.jsx
@@ -4,10 +4,10 @@ const ChatBot = () => {
   return (
     <div className='m2 mt-20 text-xs justify-center text-center shadow-blue-500/20 bg-white shadow-md rounded-2xl p-2 h-min-40 w-full'>
         ChatBot
-        <div className='border m1 text-xs justify-center shadow-blue-500/20 bg-white shadow-md rounded-2xl p-2 w-full'>
-            <p className='text-left'>Fammy: Hello! How can I assist you today?</p><br/>
-            <p className='text-right'>User: I need help with my account.</p><br/>
-            <p className='text-left'>Fammy: Sure! What seems to be the issue?</p><br/>
+        <div className='border m1 text-xs justify-center shadow-blue-500/20 bg-gray-200 shadow-md rounded-2xl p-2 w-full'>
+            <p className='text-left bg-gray-500 rounded-lg w-fit p-0.5 text-white'><span className=' font-bold'>Fammy: </span> Hello! How can I assist you today?</p><br/>
+            <p className='text-right bg-blue-500 rounded-lg p-0.5 text-white'><span className=' font-bold'>User: </span> I need help with my account.</p><br/>
+            <p className='text-left bg-gray-500 rounded-lg w-fit p-0.5 text-white'><span className='font-bold'>Fammy: </span>Sure! What seems to be the issue?</p><br/>
         </div>
     </div>
   )
