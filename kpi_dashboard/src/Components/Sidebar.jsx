baa315efc74df94c88289d0264bc80a32442ed0f
lssturdi-4 <sturdiventleroy@gmail.com>
2025-11-07 20:25:21 -0500

    updated image paths, prototype complete

--- kpi_dashboard/src/Components/Sidebar.jsx
+++ kpi_dashboard/src/Components/Sidebar.jsx
@@ -4,23 +4,23 @@ const Sidebar = () => {
   return (
     <div className='text-xs justify-center text-left font-bold shadow-blue-500/20 bg-white w-full shadow-md rounded-2xl p-2'>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="/assets/dashboard_icon.png" alt="Dashboard Icon" className='inline w-6 h-6 mr-2'/>
+            <img src="./src/Components/assets/dashboard_icon.png" alt="Dashboard Icon" className='inline w-6 h-6 mr-2'/>
             <p>Dashboard</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="/assets/white fire icon.png" alt="White Fire Icon" className='inline w-5 h-5 mr-2'/>
+            <img src="./src/Components/assets/white fire icon.png" alt="White Fire Icon" className='inline w-5 h-5 mr-2'/>
             <p>Fire Department</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="/assets/white medical icon.png" alt="Medical Icon" className='inline w-5 h-5 mr-2 center'/>
+            <img src="./src/Components/assets/white medical icon.png" alt="Medical Icon" className='inline w-5 h-5 mr-2 center'/>
             <p>Medical (EMS)</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="/assets/account_icon.png" alt="Account Icon" className='inline w-5 h-5 mr-2'/>
+            <img src="./src/Components/assets/account_icon.png" alt="Account Icon" className='inline w-5 h-5 mr-2'/>
             <p>Account</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="/assets/settings_icon.png" alt="Settings Icon" className='inline w-5 h-5 mr-2'/>
+            <img src="./src/Components/assets/settings_icon.png" alt="Settings Icon" className='inline w-5 h-5 mr-2'/>
             <p>Settings</p>
         </div>
     </div>
