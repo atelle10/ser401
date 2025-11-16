c91713da9f66a58a895531cc99b381d873013648
lssturdi-4 <sturdiventleroy@gmail.com>
2025-11-16 00:00:17 -0500

    cleaned up main.jsx file with new home component

--- kpi_dashboard/src/main.jsx
+++ kpi_dashboard/src/main.jsx
@@ -12,23 +12,5 @@ import Home from './Components/Home.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode >
       <Home />
-      {/* <div className="min-w-6xl h-screen m-2 p-2 bg-gray-200 rounded-3xl shadow-lg grid grid-cols-7 gap-1">
-        <div className="col-span-1 flex flex-col gap-2">
-          <Logo />
-          <div className="flex flex-col gap-2">
-            <Sidebar />
-            <ChatBot />
-          </div>
-        </div>
-        <div className="col-span-6 flex flex-col gap-0 h-full">
-          <div className="flex items-center w-full gap-2">
-            <div className="flex-1 pl-[6px]">
-              <NavBar />
-            </div>
-            <User />
-          </div>
-          <Dashboard className="flex-1 overflow-auto" />
-        </div>
-      </div> */}
     </StrictMode>,
 )
