9322d12f55b35591d7d59ab71f8fe625a7eb7007
lssturdi-4 <sturdiventleroy@gmail.com>
2025-11-16 20:27:41 -0500

    login page redirext to home of successful attempt - complete

--- kpi_dashboard/src/main.jsx
+++ kpi_dashboard/src/main.jsx
@@ -1,26 +1,16 @@
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
-import Login from './Components/Login.jsx'
-import { loggedInStatus } from './Components/Login.jsx'
-import Home from './Components/Home.jsx'
+import Login from './Components/Login.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
-function LoginPage() {
-  useEffect(() => {
-    console.log('Logged In Status:', loggedInStatus);
-  }, [loggedInStatus]),
-
-  root.render(
-  <StrictMode >
-    <div>
-      {loggedInStatus === true ? <Home /> : () =>(<Login />, LoginPage())}
-    </div>
-    </StrictMode>,
-  );
-}
-
-LoginPage();
+root.render(
+<StrictMode >
+  <div>
+    <Login />
+  </div>
+  </StrictMode>,
+);
 
 
