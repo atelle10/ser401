d5d793399d2f7f258c2ce2fe465643ca1101a652
lssturdi-4 <sturdiventleroy@gmail.com>
2025-11-17 22:30:01 -0500

    login UI complete

--- kpi_dashboard/src/Components/Login.jsx
+++ kpi_dashboard/src/Components/Login.jsx
@@ -1,10 +1,10 @@
-    import React, { useState } from 'react';
+    import React, { use, useState } from 'react';
     import Home from './Home';
 
     let loggedIn = false;
 
     const Login = () => {
-      const [email, setEmail] = useState('');
+      const [username, setUsername] = useState('');
       const [password, setPassword] = useState('');
 
       if (loggedIn) {
@@ -14,57 +14,59 @@
       const handleSubmit = (e) => {
         e.preventDefault();
         // Handle login logic here (e.g., send credentials to an API)
-        console.log('Email:', email, 'Password:', password);
+        console.log('Username:', username, 'Password:', password);
         loggedIn = true; // Update loggedIn status upon successful login
-        setEmail('');
+        setUsername('');
         setPassword('');
       };
 
       const handleSignUp = (e) => {
         e.preventDefault(); 
         // Handle sign-up logic here (e.g., send credentials to an API)
-        console.log('Sign Up - Email:', email, 'Password:', password);
-        setEmail('');
+        console.log('Sign Up - Username:', username, 'Password:', password);
+        setUsername('');
         setPassword('');
       };
 
       return (
-        <div className="min-w-6xl h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 via-slate-300 to-red-400">
-          <div className="p-2 bg-gray-200 rounded-3xl shadow-lg grid grid-cols-2 gap-1">
+        <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r to-blue-400 via-slate-300 from-red-400">
+          <div className='absolute inset-0 bg-[url("./src/Components/assets/login_logo.png")] bg-no-repeat bg-center opacity-70'></div>
+          <div className="p-2 bg-gray-200 rounded-3xl shadow-xl bg-transparent backdrop-blur-sm border border-white space-y-4">
             <div>
-              <img src="./src/Components/assets/Famar Logo.png" alt="Famar Logo" className="mx-auto object-center w-32 h-32" />
+              <img src="./src/Components/assets/famar_logo.png" alt="Famar Logo" className="mx-auto object-center w-32 h-32" />
             </div>
-          <div className="flex flex-col items-center justify-center bg-gray-100 p-2 rounded-2xl shadow-md">
-            <h1 className="rounded"> Login</h1>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="email">Email: </label>
-                <input
-                  className='w-24 rounded-lg border border-gray-400'
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="password">Password: </label>
-                <input
-                  className='w-24 rounded-lg border border-gray-400'
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <div className='grid grid-cols-2 gap-2 m-2'>
-                <button onClick={handleSignUp} className='px-2 py-1 rounded-lg border bg-gray-400'>Sign Up</button>
-                <button type="submit" className='px-2 py-1 rounded-lg border border-gray-400 bg-blue-400'>Log In</button>
-              </div>
-            </form>
-          </div>
+            <div className="flex flex-col items-center justify-center bg-transparent rounded-lg p-2">
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <input
+                    className='p-1 rounded-lg border bg-transparent text-white border-gray-400 m-1'
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    className='p-1 rounded-lg border bg-transparent text-white border-gray-400 m-1'
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className='flex justify-center gap-2 m-2'>
+                  <button type="submit" className='px-2 py-1 rounded-lg border border-gray-400 bg-blue-500 transition-all duration-500 ease-in-out hover:-translate-y-0.5 hover:scale-105 pointer-events-auto '>Login</button>
+                </div>
+                <div className='flex text-xs justify-center m-2'>
+                  <p>Don't have an account? <a onClick={handleSignUp} className="text-blue-700 hover:underline" href='#'>Register</a></p>
+                </div>
+              </form>
+            </div>
         </div>
       </div>
       );
