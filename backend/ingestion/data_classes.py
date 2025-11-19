a27b3f5ce5230ece65ab2b692bc716ba16676b23
Michael Krasnik <mkrasnik@asu.edu>
2025-11-19 16:36:39 -0500

    feat: added non-numeric and nan rules for the BASIC_POLICY. Policy is now enforced. DQEngine correctly checks for data inconsistencies. Good and bad files are successfully tested. df config file added.

--- backend/ingestion/data_classes.py
+++ backend/ingestion/data_classes.py
@@ -1,5 +1,19 @@
 import pandas as pd
 from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+
+class DQRule(Enum):
+    NAN = "nan"
+    NON_NUMERIC = "non_numeric"
+
+
+@dataclass
+class DQPolicy:
+    policy_id: str
+    name: str
+    rules: List[DQRule]
 
 
 @dataclass
