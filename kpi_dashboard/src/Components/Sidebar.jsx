d249978915da4c0b6280e1f9d4067d60e838dda9
Zachary Alexander <zalexan5@asu.edu>
2025-11-23 17:55:43 -0700

    Fix asset imports in Sidebar and User components
    - Import all sidebar icons (dashboard, white fire, white medical, account, settings)
    - Import account icon in User component
    - Replace string paths with ES6 imports for production build compatibility
    - Ensures all images are bundled correctly by Vite

--- kpi_dashboard/src/Components/Sidebar.jsx
+++ kpi_dashboard/src/Components/Sidebar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import dashboardIcon from './assets/dashboard_icon.png'
+import whiteFireIcon from './assets/white fire icon.png'
+import whiteMedicalIcon from './assets/white medical icon.png'
+import accountIcon from './assets/account_icon.png'
+import settingsIcon from './assets/settings_icon.png'
 
 const Sidebar = ({ currentView, setCurrentView }) => {
   const getItemClass = (view) => {
@@ -16,26 +21,26 @@ const Sidebar = ({ currentView, setCurrentView }) => {
           className={getItemClass('dashboard')}
           onClick={() => setCurrentView('dashboard')}
         >
-            <img src="./src/Components/assets/dashboard_icon.png" alt="Dashboard Icon" className='inline w-5 h-5 lg:w-6 lg:h-6 lg:mr-2'/>
+            <img src={dashboardIcon} alt="Dashboard Icon" className='inline w-5 h-5 lg:w-6 lg:h-6 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Dashboard</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="./src/Components/assets/white fire icon.png" alt="White Fire Icon" className='inline w-5 h-5 lg:mr-2'/>
+            <img src={whiteFireIcon} alt="White Fire Icon" className='inline w-5 h-5 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Fire Department</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="./src/Components/assets/white medical icon.png" alt="Medical Icon" className='inline w-5 h-5 lg:mr-2 center'/>
+            <img src={whiteMedicalIcon} alt="Medical Icon" className='inline w-5 h-5 lg:mr-2 center'/>
             <p className="hidden lg:inline whitespace-nowrap">Medical (EMS)</p>
         </div>
         <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
-            <img src="./src/Components/assets/account_icon.png" alt="Account Icon" className='inline w-5 h-5 lg:mr-2'/>
+            <img src={accountIcon} alt="Account Icon" className='inline w-5 h-5 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Account</p>
         </div>
         <div 
           className={getItemClass('settings')}
           onClick={() => setCurrentView('settings')}
         >
-            <img src="./src/Components/assets/settings_icon.png" alt="Settings Icon" className='inline w-5 h-5 lg:w-6 lg:h-6 lg:mr-2'/>
+            <img src={settingsIcon} alt="Settings Icon" className='inline w-5 h-5 lg:w-6 lg:h-6 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Settings</p>
         </div>
       </div>
--- kpi_dashboard/src/Components/User.jsx
+++ kpi_dashboard/src/Components/User.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import accountIcon from './assets/account.png'
 
 const User = () => {
   return (
     <div className="h-10 p-2 bg-white hover:bg-red-400 hover:text-white cursor-pointer rounded-full flex flex-row items-center justify-center transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 shrink shadow-blue-500/20 shadow-md w-fit">
-            <img src="./src/Components/assets/account.png" alt="Account Icon" className='inline w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 sm:mr-2'/>
+            <img src={accountIcon} alt="Account Icon" className='inline w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 sm:mr-2'/>
             <span className="hidden md:inline text-sm">John Doe</span>
     </div>
   )
