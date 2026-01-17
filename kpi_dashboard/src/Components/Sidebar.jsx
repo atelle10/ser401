2818b3faea63bb21436a0030a4784d024880c17c
lssturdi-4 <sturdiventleroy@gmail.com>
2026-01-16 19:33:18 -0500

    UI draft complete

--- kpi_dashboard/src/Components/Sidebar.jsx
+++ kpi_dashboard/src/Components/Sidebar.jsx
@@ -7,10 +7,10 @@ import settingsIcon from './assets/settings_icon.png'
 
 const Sidebar = ({ currentView, setCurrentView, onAccountClick }) => {
   const getItemClass = (view) => {
-    const baseClass = "h-8 p-2 cursor-pointer rounded-full flex justify-left items-center transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto"
+    const baseClass = "h-8 p-2 hover:text-blue-800 cursor-pointer hover:bg-white rounded-full flex justify-left items-center transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto"
     return currentView === view
-      ? `${baseClass} bg-blue-400 text-white`
-      : `${baseClass} hover:bg-blue-400`
+      ? `${baseClass} text-white`
+      : `${baseClass} hover:bg-white`
   }
 
   const handleAccountClick = () => {
@@ -20,9 +20,9 @@ const Sidebar = ({ currentView, setCurrentView, onAccountClick }) => {
       setCurrentView('account')
     }
   }
-
+ 
   return (
-    <div className='text-xs justify-center text-left font-bold shadow-blue-500/20 bg-white w-full shadow-md rounded-2xl p-2'>
+    <div className='text-xs text-white justify-center text-left font-bold shadow-blue-500/30 shadow-md rounded-2xl bg-blue-500/40 p-2'>
       {/* Desktop: Vertical list, Mobile: Horizontal scrollable */}
       <div className='flex lg:flex-col gap-2 lg:gap-4 overflow-x-auto lg:overflow-x-visible'>
         <div 
@@ -32,11 +32,11 @@ const Sidebar = ({ currentView, setCurrentView, onAccountClick }) => {
             <img src={dashboardIcon} alt="Dashboard Icon" className='inline w-5 h-5 lg:w-6 lg:h-6 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Dashboard</p>
         </div>
-        <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
+        <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-white transition-all hover:text-blue-800 duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
             <img src={whiteFireIcon} alt="White Fire Icon" className='inline w-5 h-5 lg:mr-2'/>
             <p className="hidden lg:inline whitespace-nowrap">Fire Department</p>
         </div>
-        <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-blue-400 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
+        <div className="h-8 p-2 cursor-pointer rounded-full flex justify-left items-center hover:bg-white transition-all hover:text-blue-800 duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 pointer-events-auto">
             <img src={whiteMedicalIcon} alt="Medical Icon" className='inline w-5 h-5 lg:mr-2 center'/>
             <p className="hidden lg:inline whitespace-nowrap">Medical (EMS)</p>
         </div>
