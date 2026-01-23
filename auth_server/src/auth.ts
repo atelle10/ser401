aa871d2bfdcb40f28255b7e1b38a1e2d3620d05a
atelle10 <atelle10@asu.edu>
2026-01-22 21:11:07 -0500

    feat(auth-server): enable email change in auth server

--- auth_server/src/auth.ts
+++ auth_server/src/auth.ts
@@ -22,6 +22,10 @@ export const auth = betterAuth({
   }),
   emailAndPassword: { enabled: true },
   user: {
+    changeEmail: {
+      enabled: true,
+      updateEmailWithoutVerification: true, // TODO: remove for production and enable email verification flow.
+    },
     additionalFields: {
       username: { type: "string", required: true },
       phone: { type: "string", required: true },
