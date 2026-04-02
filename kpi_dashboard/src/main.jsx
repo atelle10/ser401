4b53bf1ee373942759eecbb3bf15fd05fc6ceccf
lssturdi-4 <sturdiventleroy@gmail.com>
2026-04-01 23:50:34 -0400

    Fixing duplicate dashboard charts after rebase

--- kpi_dashboard/src/main.jsx
+++ kpi_dashboard/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import './main.css'
 import App from "./App.jsx";
+import Home from "./Components/Home.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router>
-            <App />
+            <Home />
         </Router>
     </React.StrictMode>
 );
