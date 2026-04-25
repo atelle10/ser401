2158a6112c5b23acd5786907a6f07e90b5f4ba52
EC2 Default User <ec2-user@ip-172-31-44-252.us-east-2.compute.internal>
2026-04-25 21:41:28 +0000

    ec2 deploy script fixed

--- deployment/mvp_ec2_deploy.sh
+++ deployment/mvp_ec2_deploy.sh
@@ -3,18 +3,18 @@ set -e
 
 REPO_URL="https://github.com/atelle10/ser401.git"
 BRANCH="staging"
-DEPLOY_DIR="/opt/famar"
+DEPLOY_DIR="/home/ec2-user/ser401"
 
 echo "Starting EC2 famar deployment..."
 
-echo "Installing Docker..."
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-echo "Cloning repo..."
-sudo mkdir -p $DEPLOY_DIR
-sudo chown $USER:$USER $DEPLOY_DIR
-git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
+#echo "Installing Docker..."
+#sudo apt-get update
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#
+#echo "Cloning repo..."
+#sudo mkdir -p $DEPLOY_DIR
+#sudo chown $USER:$USER $DEPLOY_DIR
+#git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR/deployment
 
@@ -24,7 +24,7 @@ if [ ! -f .env ]; then
 fi
 
 echo "Starting containers..."
-sudo docker compose up -d --build
+sudo DOCKER_BUILDKIT=0 docker compose up -d --build
 
 echo "Waiting for services to start..."
 sleep 10
